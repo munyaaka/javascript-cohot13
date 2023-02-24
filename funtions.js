50d1811a72a7b75ddcc6df715f0f4021b35b7e7d
munyaaka <munyaaka2000@gmail.com>
2023-02-24 12:30:15 +0300

    firstcommit

---
+++ funtions.js
@@ -0,0 +1,9 @@
+// a function is a reusable block of code
+
+function Add(a, b, c, r){
+    return a + b + c + r
+}
+
+Add()
+console.log('-----------------Adding values---------')
+console.log(Add(1000000, 20000000, 30000000, 20000000))
